da759bc005671ad648011a553d86243e27ab7fa7
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-06-16 12:10:23 +0200

    first commit

---
+++ TournerEnRond/TournerEnRondApp.swift
@@ -0,0 +1,17 @@
+//
+//  TournerEnRondApp.swift
+//  TournerEnRond
+//
+//  Created by dimitri on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TournerEnRondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
