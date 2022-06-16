b661e162863d545cdefb3ff8908ea8201b3530d5
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-06-16 12:15:21 +0200

    add dimview

---
+++ TournerEnRond/DimView.swift
@@ -0,0 +1,20 @@
+//
+//  DimView.swift
+//  TournerEnRond
+//
+//  Created by dimitri on 16/06/2022.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text("Dim !")
+    }
+}
+
+struct DimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimView()
+    }
+}
